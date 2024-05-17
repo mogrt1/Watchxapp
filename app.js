8e507facb348f410b1f8389c91e9164a8769b4bf
GameVerseMaster <161765401+GameVerseMaster@users.noreply.github.com>
2024-05-17 06:36:02 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+function findMyWatch() {
+    alert("Navigating to Find My Watch...");
+}
+
+function watchSettings() {
+    alert("Navigating to Watch Settings...");
+}
+
+function gettingStarted() {
+    alert("Navigating to Getting Started...");
+}
+
+function remoteInteract() {
+    alert("Navigating to Remote Interact...");
+}
